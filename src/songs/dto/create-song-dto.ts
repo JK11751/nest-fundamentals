94b4f1fc2a2ca8c7c58e12c2390fb3194fb1a70c
gimase <kgimase@gmail.com>
2024-03-31 11:30:29 +0300

    created songs DTO

---
+++ src/songs/dto/create-song-dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsDateString,
+  IsMilitaryTime,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+
+export class CreateSongDTO {
+  @IsString()
+  @IsNotEmpty()
+  readonly title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly artists: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  readonly releasedDate: Date;
+
+  @IsMilitaryTime()
+  @IsNotEmpty()
+  readonly duration: Date;
+}
